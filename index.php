8ea129b0dc4e501239dcd353fe5f127624ab40a3
hectorcruzr <35980649+hectorcruzr@users.noreply.github.com>
2022-01-06 16:31:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
-include ("inc/html.inc");
+include ("html.inc");
 echo htmll().headt("Titulo de la web| Menos de 55 caracteres").
 	metac().metanc("description","palabras clave entre 150 y 160 caracteres").
 	linkrh("stylesheet","css/estilomycodestock.css");
 echo headq().
 	body().nav();
-	include ("inc/menu.inc");  
+	include ("menu.inc");  
 	echo achtq("active","index.php"," <- este menu ya incluye su propio estilo menu.css");
 echo navq().
 	headert().h1tq("Titulo header").ptq("parrafo dentro de headers").
